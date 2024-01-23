941e48945d32de05b00c34a418d02681c322dcd2
ar83had <arshadshmim786@gmail.com>
2024-01-23 18:58:26 +0530

    second method ,bola user,factorial upto n

--- file2.c
+++ file2.c
@@ -4,14 +4,14 @@ int main(){
     printf("enter the number :");
     scanf("%d",&num);
     for(int c1=2;c1<num;c1++){
+        int flag =0;
         for(int c2=2;c2<c1;c2++){
-            if(c1 % c2 ==0)
-                break;
-            else{
-                printf("%d\t",c1);
-                break;
+            if(c1 % c2 ==0){
+                flag =1;
             }
         }
+        if(flag)
+            printf("%d\t",c1);
     }
     return 0;
 }
